5f72518f4f557f36c28b38737c0a4812f2eb095a
papsol01 <226783083+papsol01@users.noreply.github.com>
2025-08-16 00:14:43 +0200

    Initial commit

---
+++ BootIT/src/Methods/HelloWorld.java
@@ -0,0 +1,19 @@
+package Methods;
+
+public class HelloWorld {
+    public void printHelloWorld() {
+        // Prints out "Hello world!"
+        /*
+    Prints out "Hello World!"
+     */
+        System.out.println();
+        System.out.println("Hello world!");
+        System.out.println();
+        System.out.println("X    X   XXXX   X       X        XXX");
+        System.out.println("X    X   X      X       X       X   X");
+        System.out.println("XXXXXX   XXX    X       X       X   X");
+        System.out.println("X    X   X      X       X       X   X");
+        System.out.println("X    X   XXXX   XXXXX   XXXXX    XXX");
+        System.out.println();
+    }
+}
---
+++ FizzBuzzTest/src/FizzBuzzTestForEachLoop.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class FizzBuzzTestForEachLoop {
+
+    public void runFizzBuzzTestForEachLoop() {
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        for (int i = 1; i <= 100; i++) {
+            numbers.add(i);
+        }
+
+        for (Integer i : numbers) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzzTest");
+            }
+            else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            }
+            else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            }
+            else {
+                System.out.println(i);
+            }
+        }
+
+    }
+
+
+}
